cf60627a2b5df2d31933db06013c00c5392dd546
Sangay81 <03230258.gcbs@rub.edu.bt>
2024-03-11 12:53:56 +0600

    first commit

---
+++ dataTypes.py
@@ -0,0 +1,19 @@
+x = "Hello World"
+x = 50
+x = 60.5
+x = 3j
+x = ["geeks", "for", "geeks"]
+x = ("geeks ", "for", "geeks")
+x = range (10)
+x = {"name": "suraj", "age":24}
+x = {"geeks", "for", "geeks"}
+x = frozenset({"geeks", "for", "geeks"})
+x = True
+x = b"geeks"
+x = bytearray(4)
+x = memoryview(bytes(6))
+x = None
+
+my_list = []
+my_list.insert(0,0)
+print(my_list) 
